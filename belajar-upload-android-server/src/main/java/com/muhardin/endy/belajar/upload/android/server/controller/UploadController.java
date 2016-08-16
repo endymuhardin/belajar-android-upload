de6cd1982f598879817888d6fb14a5f1b488f3af
Endy Muhardin <endy.muhardin@gmail.com>
2016-08-16 13:11:56 +0700

    implement get file

--- belajar-upload-android-server/src/main/java/com/muhardin/endy/belajar/upload/android/server/controller/UploadController.java
+++ belajar-upload-android-server/src/main/java/com/muhardin/endy/belajar/upload/android/server/controller/UploadController.java
@@ -10,27 +10,33 @@ import java.util.Map;
 import java.util.UUID;
 import java.util.logging.Level;
 import java.util.stream.Collectors;
-import javax.activation.MimetypesFileTypeMap;
 import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
-import org.springframework.util.MimeType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-@Controller
+@RestController
 public class UploadController {
 
     public static final String UPLOAD_FOLDER = "uploads";
     private static final Logger LOGGER = LoggerFactory.getLogger(UploadController.class);
 
+    private final ResourceLoader resourceLoader;
+
+    @Autowired
+    public UploadController(ResourceLoader resourceLoader) {
+        this.resourceLoader = resourceLoader;
+    }
+
     @RequestMapping(method = RequestMethod.GET, value = "/")
-    @ResponseBody
     public List<Map<String, Object>> listUploads(HttpServletRequest req) throws IOException {
         return Files.walk(Paths.get(UPLOAD_FOLDER))
                 .filter(path -> !path.equals(Paths.get(UPLOAD_FOLDER)))
@@ -38,27 +44,45 @@ public class UploadController {
                 .collect(Collectors.toList());
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "/")
-    public String handleFileUpload(@RequestParam("file") MultipartFile file,
-            RedirectAttributes redirectAttributes) {
+    @RequestMapping(method = RequestMethod.GET, value = "/uploads/{filename:.+}")
+    public ResponseEntity<?> getFile(@PathVariable String filename) {
 
+        try {
+            return ResponseEntity
+                    .ok(resourceLoader.getResource("file:" + Paths.get(UPLOAD_FOLDER, filename).toString()));
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage(), e);
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/")
+    public Map<String, Object> handleFileUpload(@RequestParam("file") MultipartFile file,
+            HttpServletRequest req) {
+
+        Map<String, Object> hasil = new LinkedHashMap<>();
         if (!file.isEmpty()) {
             try {
                 String newFilename = UUID.randomUUID().toString() + "."
-                        +file.getOriginalFilename()
-                                .substring(file.getOriginalFilename().lastIndexOf('.')+1);
+                        + file.getOriginalFilename()
+                        .substring(file.getOriginalFilename().lastIndexOf('.') + 1);
                 Files.copy(file.getInputStream(), Paths.get(UPLOAD_FOLDER, newFilename));
                 LOGGER.info("You successfully uploaded {} to {}", file.getOriginalFilename(), newFilename);
+                hasil.put("success", true);
+                hasil.put("url", Paths.get(UPLOAD_FOLDER, newFilename));
             } catch (IOException | RuntimeException e) {
                 LOGGER.error(e.getMessage(), e);
+                hasil.put("success", false);
+                hasil.put("error", e.getMessage());
             }
         } else {
             LOGGER.error("File is empty");
+            hasil.put("success", false);
+            hasil.put("error", "File is empty");
         }
-
-        return "redirect:/";
+        return hasil;
     }
-    
+
     private Map<String, Object> pathToMap(Path p) {
         try {
             Map<String, Object> hasil = new LinkedHashMap<>();
