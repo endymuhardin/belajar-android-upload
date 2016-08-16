c344e06c80d613632df1e87ba0672bdc835a6235
Endy Muhardin <endy.muhardin@gmail.com>
2016-08-16 11:15:44 +0700

    update to java 8 stream

--- belajar-upload-android-server/src/main/java/com/muhardin/endy/belajar/upload/android/server/controller/UploadController.java
+++ belajar-upload-android-server/src/main/java/com/muhardin/endy/belajar/upload/android/server/controller/UploadController.java
@@ -1,10 +1,11 @@
 package com.muhardin.endy.belajar.upload.android.server.controller;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
-import java.util.Map;
+import java.util.stream.Collectors;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,23 +16,10 @@ public class UploadController {
     public static final String UPLOAD_FOLDER = "uploads";
     
     @RequestMapping(method = RequestMethod.GET, value = "/list")
-    public List<Map<String, String>> listUploads(HttpServletRequest req){
-        Map<String, String> fileInfo = new LinkedHashMap<>();
-        String namaFile = "contoh-file.jpg";
-        
-        fileInfo.put("nama", namaFile);
-        
-        String url = req.getContextPath()+UPLOAD_FOLDER+"/"+namaFile;
-        fileInfo.put("url", url);
-        
-        String realPath = req.getServletContext().getRealPath(UPLOAD_FOLDER);
-        String path = realPath + File.separator + namaFile;
-        fileInfo.put("path", path);
-        fileInfo.put("ukuran", "123KB");
-        
-        List<Map<String, String>> daftarFile = new ArrayList<>();
-        daftarFile.add(fileInfo);
-        
-        return daftarFile;
+    public List<Path> listUploads(HttpServletRequest req) throws IOException {
+        return Files.walk(Paths.get(UPLOAD_FOLDER))
+                .filter(path -> !path.equals(Paths.get(UPLOAD_FOLDER)))
+                .map(path -> Paths.get(UPLOAD_FOLDER).relativize(path))
+                .collect(Collectors.toList());
     }
 }
